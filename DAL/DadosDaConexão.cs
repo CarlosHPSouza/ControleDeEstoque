e9bb90678a5ba6535b81b3861430b01fe2294b7c
Carlos <carlos.chps@outlook.com>
2020-06-22 14:49:44 -0300

    Add project files.

---
+++ DAL/DadosDaConexão.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DadosDaConexão
+    {
+        public static String StringDeConexao
+        {
+            get 
+            {
+                return "Data Source=DESKTOP-MHRFVSS\\SQLEXPRESS;Initial Catalog=ControleDeEstoque;Integrated Security=True";
+            }
+        }
+
+    }
+}
